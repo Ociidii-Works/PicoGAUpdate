ff45ea0d5ab63cb7f20d07e1d64f64e5153de9c2
XenHat <commits@xenh.at>
2020-09-22 01:10:52 -0400

    re-order source directory a little

---
+++ Src/Components/CheckAdmin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace PicoGAUpdate
+{
+    internal class CheckAdmin
+    {
+        private static readonly WindowsIdentity Identity = WindowsIdentity.GetCurrent();
+        private static readonly WindowsPrincipal Principal = new WindowsPrincipal(Identity ?? throw new InvalidOperationException());
+
+        public static bool IsElevated
+        {
+            get
+            {
+                Debug.Assert(Principal != null, nameof(Principal) + " != null");
+                return Principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+        }
+    }
+}
